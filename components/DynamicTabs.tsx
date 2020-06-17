5d306452b8f7c07070040e2ea4abd65a28387c2c
Elizabeth Vincent M <elizabethvincentm@gmail.com>
2020-06-17 15:51:23 +0530

    feat: break down DynamicTabs to subcomponents

--- components/DynamicTabs.tsx
+++ components/DynamicTabs.tsx
@@ -1,27 +1,9 @@
 import React, { useState } from 'react'
 import { View, Text, TouchableOpacity, Modal } from 'react-native'
 import { Styles } from '../Styles'
-import MenuIcon from '../assets/menu.svg'
+import { DynamicTabsProps } from '../types/DynamicTabsTypes'
+import { TabBar, TabModal } from '../components'
 
-type DynamicTabConfig = {
-  // title of the tab - tab name
-  title: string
-  // Component to mount/show on the tab screen when tab is active
-  component: React.ComponentType
-}
-
-interface DynamicTabsProps {
-  // tab configuration
-  config: Array<DynamicTabConfig>
-  // max tabs to show on the screen at a time
-  maxTabsToShow: number
-}
-
-const updateTabComponent = (TabComponent: React.ComponentType) => {
-  return () => {
-    return <TabComponent />
-  }
-}
 export const DynamicTabs: React.FC<DynamicTabsProps> = ({
   config,
   maxTabsToShow,
@@ -29,75 +11,29 @@ export const DynamicTabs: React.FC<DynamicTabsProps> = ({
   const [selectedTabIndex, setSelectedTabIndex] = useState<number>(0)
   const [lastTab, setLastTab] = useState<number>(maxTabsToShow)
   const [allTabsVisible, setAllTabsVisible] = useState<boolean>(false)
-  const EnhancedComponent = updateTabComponent(
-    config[selectedTabIndex].component
-  )
+  const tabBarProps = {
+    config,
+    maxTabsToShow,
+    lastTab,
+    selectedTabIndex,
+    setSelectedTabIndex,
+    setAllTabsVisible,
+  }
+  const tabModalProps = {
+    config,
+    maxTabsToShow,
+    allTabsVisible,
+    setLastTab,
+    setSelectedTabIndex,
+    setAllTabsVisible,
+  }
+  const TabComponent = config[selectedTabIndex].component
   return (
     <View style={Styles.dynamicTabView}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={allTabsVisible}
-        onRequestClose={() => {
-          setAllTabsVisible(false)
-        }}
-      >
-        <TouchableOpacity
-          style={{
-            backgroundColor: '#00000080',
-            flex: 1,
-          }}
-          onPress={() => setAllTabsVisible(false)}
-        >
-          {config.map((tab, tabIndex) => (
-            <TouchableOpacity
-              style={Styles.dynamicTabModalView}
-              key={tabIndex}
-              onPress={() => {
-                setLastTab(() =>
-                  tabIndex >= maxTabsToShow ? tabIndex + 1 : maxTabsToShow
-                )
-                setSelectedTabIndex(tabIndex)
-                setAllTabsVisible(false)
-              }}
-            >
-              <Text style={Styles.dynamicTabModalText}>{tab.title}</Text>
-            </TouchableOpacity>
-          ))}
-        </TouchableOpacity>
-      </Modal>
-
-      <View style={Styles.dynamicTabHeader}>
-        {config.slice(lastTab - maxTabsToShow, lastTab).map((tab, tabIndex) => (
-          <TouchableOpacity
-            style={{
-              ...Styles.dynamicTabHeaderItemView,
-              borderBottomWidth:
-                tabIndex + lastTab - maxTabsToShow === selectedTabIndex ? 2 : 0,
-
-              borderBottomColor:
-                tabIndex + lastTab - maxTabsToShow === selectedTabIndex
-                  ? '#255A80'
-                  : '#4BB4FF',
-            }}
-            key={tabIndex}
-            onPress={() =>
-              setSelectedTabIndex(tabIndex + lastTab - maxTabsToShow)
-            }
-          >
-            <Text style={Styles.dynamicTabHeaderItemText}>{tab.title}</Text>
-          </TouchableOpacity>
-        ))}
-
-        <TouchableOpacity
-          style={Styles.dynamicTabHeaderMenuIcon}
-          onPress={() => setAllTabsVisible(true)}
-        >
-          <MenuIcon />
-        </TouchableOpacity>
-      </View>
+      <TabModal {...tabModalProps} />
+      <TabBar {...tabBarProps} />
       <View style={Styles.dynamicTabComponentView}>
-        <EnhancedComponent />
+        <TabComponent />
       </View>
     </View>
   )
