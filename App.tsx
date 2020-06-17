ef97e6bf8ebcda37371c2725ba399262184279cf
Elizabeth Vincent M <elizabethvincentm@gmail.com>
2020-06-17 13:36:58 +0530

    feat:add typescript interfaces

--- App.tsx
+++ App.tsx
@@ -9,41 +9,43 @@
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
 import { DynamicTabs } from './components'
-import { Styles } from './Styles'
 
 const App = () => {
-  const data = {
+  const tabData = {
     config: [
       {
         title: 'Tab1',
-        component: <Text>Tab1 Data</Text>,
+        component: () => <Text>Tab1 Data</Text>,
       },
       {
         title: 'Tab2',
-        component: <Text>Tab2 Data</Text>,
+        component: () => <Text>Tab2 Data</Text>,
       },
       {
         title: 'Tab3',
-        component: <Text>Tab3 Data</Text>,
+        component: () => <Text>Tab3 Data</Text>,
       },
       {
         title: 'Tab4',
-        component: <Text>Tab4 Data</Text>,
+        component: () => <Text>Tab4 Data</Text>,
       },
       {
         title: 'Tab5',
-        component: <Text>Tab5 Data</Text>,
+        component: () => <Text>Tab5 Data</Text>,
       },
       {
         title: 'Tab6',
-        component: <Text>Tab6 Data</Text>,
+        component: () => <Text>Tab6 Data</Text>,
       },
     ],
-    maxTabToShow: 5,
+    maxTabsToShow: 5,
   }
   return (
-    <SafeAreaView styles={Styles.container}>
-      <DynamicTabs DynamicTabsProps={data} />
+    <SafeAreaView>
+      <DynamicTabs
+        config={tabData.config}
+        maxTabsToShow={tabData.maxTabsToShow}
+      />
     </SafeAreaView>
   )
 }
