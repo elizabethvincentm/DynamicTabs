c5f2fbd0c94554d973b1b82079da186c8b9e062b
Elizabeth Vincent M <elizabethvincentm@gmail.com>
2020-06-17 10:25:34 +0530

    feat:add menu icon

--- components/DynamicTabs.js
+++ components/DynamicTabs.js
@@ -1,58 +1,81 @@
 import React, { useState } from 'react'
 import { View, Text, TouchableOpacity, Modal } from 'react-native'
+
+import { Styles } from '../Styles'
 import MenuIcon from '../assets/menu.svg'
 
 export const DynamicTabs = ({ DynamicTabsProps }) => {
   const { config, maxTabToShow } = DynamicTabsProps
   const [selectedTabIndex, setSelectedTabIndex] = useState(0)
   const [lastTab, setLastTab] = useState(maxTabToShow)
-  const [modalVisible, setModalVisible] = useState(false)
+  const [allTabsVisible, setAllTabsVisible] = useState(false)
   return (
-    <View>
+    <View style={Styles.dynamicTabView}>
       <Modal
-        animationType="fade"
-        // transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {}}
+        animationType="slide"
+        transparent={true}
+        visible={allTabsVisible}
+        onRequestClose={() => {
+          setAllTabsVisible(false)
+        }}
       >
-        {config.map((tab, tabIndex) => (
-          <TouchableOpacity
-            key={tabIndex}
-            onPress={() => {
-              setLastTab(() =>
-                tabIndex >= maxTabToShow ? tabIndex + 1 : maxTabToShow
-              )
-              setSelectedTabIndex(tabIndex)
-              setModalVisible(false)
-            }}
-          >
-            <Text>{tab.title}</Text>
-          </TouchableOpacity>
-        ))}
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#00000080',
+            flex: 1,
+          }}
+          onPress={() => setAllTabsVisible(false)}
+        >
+          {config.map((tab, tabIndex) => (
+            <TouchableOpacity
+              style={Styles.dynamicTabModalView}
+              key={tabIndex}
+              onPress={() => {
+                setLastTab(() =>
+                  tabIndex >= maxTabToShow ? tabIndex + 1 : maxTabToShow
+                )
+                setSelectedTabIndex(tabIndex)
+                setAllTabsVisible(false)
+              }}
+            >
+              <Text style={Styles.dynamicTabModalText}>{tab.title}</Text>
+            </TouchableOpacity>
+          ))}
+        </TouchableOpacity>
       </Modal>
 
-      <View style={{ flexDirection: 'row' }}>
-        <View style={{ flexDirection: 'row' }}>
-          {config
-            .slice(lastTab - maxTabToShow, lastTab)
-            .map((tab, tabIndex) => (
-              <TouchableOpacity
-                key={tabIndex}
-                onPress={() =>
-                  setSelectedTabIndex(tabIndex + lastTab - maxTabToShow)
-                }
-              >
-                <Text>{tab.title}</Text>
-              </TouchableOpacity>
-            ))}
-        </View>
+      <View style={Styles.dynamicTabHeader}>
+        {config.slice(lastTab - maxTabToShow, lastTab).map((tab, tabIndex) => (
+          <TouchableOpacity
+            style={{
+              ...Styles.dynamicTabHeaderItemView,
+              borderBottomWidth:
+                tabIndex + lastTab - maxTabToShow === selectedTabIndex ? 2 : 0,
 
-        {/* <MenuIcon /> */}
-        <TouchableOpacity onPress={() => setModalVisible(true)}>
-          <Text>menu</Text>
+              borderBottomColor:
+                tabIndex + lastTab - maxTabToShow === selectedTabIndex
+                  ? '#255A80'
+                  : '#4BB4FF',
+            }}
+            key={tabIndex}
+            onPress={() =>
+              setSelectedTabIndex(tabIndex + lastTab - maxTabToShow)
+            }
+          >
+            <Text style={Styles.dynamicTabHeaderItemText}>{tab.title}</Text>
+          </TouchableOpacity>
+        ))}
+
+        <TouchableOpacity
+          style={Styles.dynamicTabHeaderMenuIcon}
+          onPress={() => setAllTabsVisible(true)}
+        >
+          <MenuIcon />
         </TouchableOpacity>
       </View>
-      <View>{config[selectedTabIndex].component}</View>
+      <View style={Styles.dynamicTabComponentView}>
+        {config[selectedTabIndex].component}
+      </View>
     </View>
   )
 }
